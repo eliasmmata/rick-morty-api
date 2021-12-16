5ef2783872e5a66f5a225ce406a9a4ea56c1f1e6
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2021-12-16 11:32:07 +0100

    heroku node

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './core/menu/menu.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptProviders } from './shared/http-interceptors';
 import { SpinnerComponent } from './core/spinner/spinner.component';
 @NgModule({

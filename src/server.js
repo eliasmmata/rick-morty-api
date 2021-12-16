9e64460f3a935fb674a050c5dfa97a9be0dab741
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2021-12-16 11:10:27 +0100

    heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-rick-morty'));
+app.use(express.static(__dirname+'/dist/rick-morty-project'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/angular-rick-morty/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/rick-morty-project/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);

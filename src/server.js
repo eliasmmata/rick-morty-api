24ca84dce04e9618e5c7337fb5f6e0799f959931
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2021-12-16 10:44:26 +0100

    actualizaciones para heroku

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = express();
+
+app.use(express.static(__dirname+'/dist/angular-rick-morty'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/angular-rick-morty/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
